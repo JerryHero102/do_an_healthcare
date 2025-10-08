2c20bc068538b560aa4875fa3371afc059ada773
MaiVanKhanh <maivankhanh2608@gmail.com>
2025-10-08 12:19:08 +0700

    Commit {FontEnds, BackEnds File; Admin/DashBoard}

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import Header from './components/Header'
-import Body from './components/Body'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Body from './components/body';
+import DashBoard from './FontEnds/Admin/DashBoard'; 
+
 function App() {
   return (
-    <>
-      <div>
-        <Header/>
-        <Body/>
-      </div>
-    </>
-  )
+    <BrowserRouter>
+        <Routes>
+        <Route path="/" element={<Header />} />
+        <Route path="/" element={<Body />} />
+        <Route path="/Admin" element={<DashBoard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,9 @@
-const Body = () => {
-    return (
-        <div className="flex justify-center items-center h-[500px] bg-[#FFF3D1]">
-            <h1 className="text-4xl font-bold text-gray-800">Welcome to HealthCare</h1>
-        </div>
-    );
+const body = () => {
+  return (
+    <div>body</div>
+  )
 }
-export default Body
+
+export default body
+
+// export nhanh 'rafce'
