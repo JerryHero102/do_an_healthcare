667b61fe463729f0665c5ace29d2b5dfa9ec6f92
MaiVanKhanh <maivankhanh2608@gmail.com>
2025-10-10 07:58:46 +0700

    update folder Benh Nhanh

--- src/FontEnds/Admin/QLBenhNhan/QLBN_Select_QL.jsx
+++ src/FontEnds/Admin/QLBenhNhan/QLBN_Select_QL.jsx
@@ -1,41 +1,39 @@
 
 const ButtonPrimary = ({ children, onClick }) => (
-  <button 
-    className="text-2xl w-[500px] h-[150px] bg-white rounded-md m-8 flex items-center justify-center hover:cursor-pointer hover:bg-[#FFF9E9] active:bg-[#FFC419] active:border-0"
-    onClick={onClick}
-  >
-    {children}
-  </button>
+    <button 
+        className="text-2xl w-[500px] h-[150px] bg-white rounded-md m-8 flex items-center justify-center hover:cursor-pointer hover:bg-[#FFF9E9] active:bg-[#FFC419] active:border-0"
+        onClick={onClick}>{children}
+    </button>
 );
 
 // 💡 Nhận setContext làm prop
 const QLBN_Select_QL = ({ setContext }) => {
-  return (
-    <div className='w-full h-full bg-gray-100 p-4 flex flex-col items-center justify-center'>
+ return (
+    <div className='w-full h-full bg-gray-100 p-4 flex flex-col items-center justify-center'>
         
-        <ButtonPrimary 
+         <ButtonPrimary 
             //Nhấn vào sẽ thay thế context thành "Thêm Bệnh Nhân"
             onClick={() => setContext("Thêm bệnh nhân mới")}
         >
             Thêm bệnh nhân mới
         </ButtonPrimary>
         
-        <ButtonPrimary 
+         <ButtonPrimary 
             //Nhấn vào sẽ thay thế context thành "BN Chưa Khám"
             onClick={() => setContext("Danh sách bệnh nhân chưa khám")}
         >
             Danh sách bệnh nhân chưa khám bệnh
         </ButtonPrimary>
         
-        <ButtonPrimary 
+        <ButtonPrimary 
             //Nhấn vào sẽ thay thế context thành "BN Đã Khám"
             onClick={() => setContext("BN Đã Khám")}
         >
             Danh sách bệnh nhân đã khám bệnh
         </ButtonPrimary>
         
-    </div>  
-  )
+    </div> 
+ )
 }
 
 export default QLBN_Select_QL;
